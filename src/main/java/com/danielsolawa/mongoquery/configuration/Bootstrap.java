33fb61526deaab2b64919f56cc69798094657431
Daniel Solawa <>
2021-04-09 15:40:07 +0200

    minor fixes

--- src/main/java/com/danielsolawa/mongoquery/configuration/Bootstrap.java
+++ src/main/java/com/danielsolawa/mongoquery/configuration/Bootstrap.java
@@ -1,33 +1,35 @@
 package com.danielsolawa.mongoquery.configuration;
 
+import com.danielsolawa.mongoquery.example.model.Dog;
 import com.danielsolawa.mongoquery.example.model.Human;
 import com.danielsolawa.mongoquery.example.repository.HumanRepository;
 import com.danielsolawa.mongoquery.example.specification.HumanMongoSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.domain.Page;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
-    private final MongoTemplate mongoTemplate;
     private final HumanRepository humanRepository;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Bootstrap.class.getName());
 
-    public Bootstrap(MongoTemplate mongoTemplate, HumanRepository humanRepository) {
-        this.mongoTemplate = mongoTemplate;
+    public Bootstrap(HumanRepository humanRepository) {
         this.humanRepository = humanRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 //        Human human = new Human();
-//        human.setName("Adam");
+//        human.setName("Daniel");
+//        human.setDogList(Collections.singletonList(new Dog("ace")));
 //
 //
 //        humanRepository.save(human);
@@ -35,13 +37,13 @@ public class Bootstrap implements CommandLineRunner {
 
         HumanMongoSpecification humanMongoSpecification = new HumanMongoSpecification();
         humanMongoSpecification.setPageNumber(0);
-        humanMongoSpecification.setPageSize(1);
-//        humanMongoSpecification.setSortBy(Collections.singletonList("name;desc"));
-//        Page<Human> all = humanRepository.findAll(humanMongoSpecification, humanMongoSpecification.getPageRequest());
+        humanMongoSpecification.setPageSize(2);
+        humanMongoSpecification.setSortBy(Collections.singletonList("dogList.name;desc"));
+        Page<Human> all = humanRepository.findAll(humanMongoSpecification, humanMongoSpecification.getPageRequest());
 
-        List<Human> humanList = humanRepository.findAll(humanMongoSpecification);
+//        List<Human> humanList = humanRepository.findAll(humanMongoSpecification);
 
-        LOGGER.info("[run] {}", humanList);
+        LOGGER.info("[run] {}", all.getContent());
     }
 
 
--- src/main/java/com/danielsolawa/mongoquery/example/model/Human.java
+++ src/main/java/com/danielsolawa/mongoquery/example/model/Human.java
@@ -2,12 +2,18 @@ package com.danielsolawa.mongoquery.example.model;
 
 import org.springframework.data.annotation.Id;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Human {
 
     @Id
     private String id;
 
     private String name;
+
+    private List<Dog> dogList = new ArrayList<>();
+
 
     public String getId() {
         return id;
@@ -25,11 +31,20 @@ public class Human {
         this.name = name;
     }
 
+    public List<Dog> getDogList() {
+        return dogList;
+    }
+
+    public void setDogList(List<Dog> dogList) {
+        this.dogList = dogList;
+    }
+
     @Override
     public String toString() {
         return "Human{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
+                ", dogList=" + dogList +
                 '}';
     }
 }
